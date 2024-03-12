0ac649c2c5815d57ed7b8b8946f89db400f99014
ayrtont1998 <143474841+ayrtont1998@users.noreply.github.com>
2024-03-12 00:37:35 -0300

    then, i created the "playRound" function

--- javascript/script.js
+++ javascript/script.js
@@ -10,26 +10,26 @@ function getComputerChoice() {
     }
 }
 
-// function capitalize(text) {
-//    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
-// } 
+function capitalize(text) {
+   return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+} 
 
-// function playRound(playerSelection, computerSelection){
-//     playerSelection = capitalize(prompt("What you choice?", ''));
-//     console.log("Your choise: " + playerSelection);
-//     computerSelection = getComputerChoice();
-//     console.log("Computer choise: " + computerSelection);
-//     if (playerSelection === computerSelection) {
-//         return "Tie"
-//     } else if (playerSelection === "Scissors" && computerSelection === "Paper"
-//             || playerSelection === "Rock" && computerSelection === "Scissors"
-//             || playerSelection === "Paper" && computerSelection === "Rock"){
-//                 return `You win!${playerSelection} beats ${computerSelection}`;
-//             }
-//         else {
-//             return `You lose!${computerSelection} beats ${playerSelection}`;
-//         }
-// }
+function playRound(playerSelection, computerSelection){
+    playerSelection = capitalize(prompt("What you choice?", ''));
+    console.log("Your choise: " + playerSelection);
+    computerSelection = getComputerChoice();
+    console.log("Computer choise: " + computerSelection);
+    if (playerSelection === computerSelection) {
+        return "Tie"
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper"
+            || playerSelection === "Rock" && computerSelection === "Scissors"
+            || playerSelection === "Paper" && computerSelection === "Rock"){
+                return `You win!${playerSelection} beats ${computerSelection}`;
+            }
+        else {
+            return `You lose!${computerSelection} beats ${playerSelection}`;
+        }
+}
 
 
 // function playGame(){
